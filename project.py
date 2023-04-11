248a7570f5f2b76fc0360ebf66cbdb5d27e3dabb
DonaldDuckEde <olivier.hartsuiker@gmail.com>
2023-04-11 11:41:00 +0200

    changes

--- project.py
+++ project.py
@@ -7,4 +7,31 @@ def fileCopie():
     shutils.copyfile('main.csv', 'data.csv')
 
 def changeData():
+    df = pd.read_csv('data.csv')
+
+    df['Time'] = df['Time'].str.split().str[1]
+
+    df['Time'] = pd.to_datetime(df['Time'], format='%H:%M:%S')
+
+    df['Time'] = df['Time'].dt.round('10min')
+
+    df.to_csv('data.csv', index=False)
+
+    df = pd.read_csv('data.csv')
+
+    df.iloc[:, 0] = df.iloc[:, 0].apply(lambda x: x.split()[-1])
+
+    df.to_csv('data.csv', index=False)
+
+def getId():
+    try:
+        user_input = input("Enter an ID: ")
+        return user_input
+    except:
+        print("Something went terribly wrong")
+
+def main():
+    fileCopie()
+    getId()
+    changeData()
     
